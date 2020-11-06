16cd71160ceb9922dd7550f70a362cd9022f86fc
VictorLaureano <victor.laureano>
2020-11-06 18:37:55 -0400

    Added some comments

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { Result } from 'antd';
 import React from 'react';
 import './App.css';
 
+//Api implementation was made by using NewsApi Module.
 class App extends React.Component{
   constructor(props){
     super(props);
